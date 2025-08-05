b7c1710a5479bc7d6f4b596ceac0ca0a2b49f35d
agrawaleshika07-dev <agrawal.eshika07@gmail.com>
2025-08-05 23:08:06 +0530

    Initial commit from ServiceNow

---
+++ src/main/java/com/example/dbgittry/Message.java
@@ -0,0 +1,29 @@
+package com.example.dbgittry;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String content;
+
+    public Message() {}
+
+    public Message(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
