b0b14d98d86384f82fedb68d764f9c64cb5db603
agrawaleshika07-dev <agrawal.eshika07@gmail.com>
2025-08-05 23:08:05 +0530

    Initial commit from ServiceNow

---
+++ src/main/java/com/example/dbgittry/Main.java
@@ -0,0 +1,13 @@
+package com.example.dbgittry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+}
